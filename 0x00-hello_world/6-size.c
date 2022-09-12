6b5032917d4658d699c61f010f0a4ae7bd48d810
Anthonia Godwin <giantonia001@gmail.com>
2022-09-12 11:35:56 -0700

    Territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Print C program that prints various types on the computer
+ * main - Print "C program that prints various types on the computer"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of char: %1u byte(s)\n", sizeof(char));
-	printf("Size of int: %1u byte(s)\n", sizeof(int));
-	printf("Size of long int: %1u byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %1u byte(s)\n", sizeof(long long int));
-	printf("Size of float: %1u byte(s)\n", sizeof(float));
+	printf("Size of char: %1u byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of long int: %1u byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of long long int: %1u byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of float: %1u byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
