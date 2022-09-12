1e06edbe08b53d30ab49fca7d919fa6fe572a915
Anthonia Godwin <giantonia001@gmail.com>
2022-09-12 03:36:52 -0700

    Territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the size of various types that run the C language
+ * main - Print the size of various types computers that run the C language
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
-
