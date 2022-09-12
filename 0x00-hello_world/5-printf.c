9d0fada3488d24d969284515bbf4753e8f354841
Anthonia Godwin <giantonia001@gmail.com>
2022-09-11 17:52:18 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-# include <stdio.h>                                                                                                                             
-/**                                                                                                                                             
- * main - Printf "with proper grammar, but the outcome is a piece of art"
+#include <stdio.h>
+
+/**
+ * main - Print "with proper grammar, but the outcome is a piece of art"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
+
