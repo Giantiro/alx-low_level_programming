0a2cb8b74bcc88e44f7ce1ee8aaa73b6ee5eef85
Anthonia Godwin <giantonia001@gmail.com>
2022-09-14 10:35:30 -0700

    If character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - tests for English alphabet
+ * @c: character to be checked
+ * Return: 1 if chaaracter is an English alphabet
+ * 0 if character is not an alphabet
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	Else
+		return (0);
+}
