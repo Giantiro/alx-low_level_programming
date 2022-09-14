dca8dc3c1132558f35f955cfda3da8e7f28f4532
Anthonia Godwin <giantonia001@gmail.com>
2022-09-14 05:56:38 -0700

    Print without standard lib

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Write a program that prints "_putchar", followed by a new line
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
