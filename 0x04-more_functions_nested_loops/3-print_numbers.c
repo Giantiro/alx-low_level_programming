2356236b8f88b3cc651b691098d654bed3a3a8de
Anthonia Godwin <giantonia001@gmail.com>
2022-09-16 09:49:02 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
  * print_numbers - prints 0123456789\n
  * Return: void
  */
+
 void print_numbers(void)
 {
-	int a = 0;
-
-	for (a = 48; a <= 58; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a + '0');
+		_putchar(a);
 	}
 	_putchar('\n');
 }
