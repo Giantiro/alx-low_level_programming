c880e67a5f275b53c9455a3a2c98bcca648762da
Anthonia Godwin <giantonia001@gmail.com>
2022-09-16 09:27:21 -0700

    Decimal numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
  * print_numbers - prints 0123456789\n
- *
  * Return: void
  */
-
-
 void print_numbers(void)
 {
-	char a;
+	int a;
 
-	for (a = 0; a <= 9; a++)
+	while a <= 9
 	{
-		_putchar((char)a);
+		_putchar(a + '0');
+		a++
 	}
 	_putchar('\n');
 }
