c79208e2bb8cdf0fdff7e5bf8638045824763ec3
Anthonia Godwin <giantonia001@gmail.com>
2022-10-10 15:18:42 -0700

    Initialize type stuct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,44 +1,20 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * print_dog - Print out all of the struct dog info
- * @d: Pointer to struct
+ * init_dog - initializes a dog
+ * @d: the dog to init
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Return: void.
  */
-void print_dog(struct dog *d)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
-}
-#include <stdio.h>
-#include "dog.h"
-
-/**
- * print_dog - Print out all of the struct dog info
- * @d: Pointer to struct
- */
-void print_dog(struct dog *d)
-{
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
