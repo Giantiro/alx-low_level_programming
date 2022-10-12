2d942de9795eb6834507a227507241830b987547
Anthonia Godwin <giantonia001@gmail.com>
2022-10-12 04:38:02 -0700

    Function pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+
+#endif
